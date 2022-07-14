ba3e9f4f836c67917074255af23e46be3f005828
vladimirovicp <vladimirovicp70@gmail.com>
2022-07-14 10:45:54 +0400

    feat: final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    function removingUnwantedCharacters(item) { //удаление нежелательных символов
+        let i = 0;
+        found = false;
+
+        while (i < bracketsConfig.length && !found) {
+            item === bracketsConfig[i][0] || item === bracketsConfig[i][1] ? found = true : found = false;
+            i++;
+        }
+        return found;
+    }
+
+    const masStr = str.split('');
+
+    if (masStr.length % 2 !== 0) {
+        return false;
+    }
+
+    let masStrFilter = masStr.filter((item) => {
+        return removingUnwantedCharacters(item);
+    });
+
+
+    let rez = [];
+    let i = 0;
+    let stop = false;
+
+    while (i < masStrFilter.length && !stop) {
+        bracketsConfig.forEach((value, index) => {
+            if (masStrFilter[i] === value[0] || masStrFilter[i] === value[1]) {
+
+                if (rez.length === 0) {
+                    masStrFilter[i] === value[0] ? rez.push(masStrFilter[i]) : stop = true;
+                } else {
+
+                    if (masStrFilter[i] === value[0] && value[0] != value[1]) {
+
+                        rez.push(masStrFilter[i])
+                    } else {
+                        rez[rez.length - 1] === value[0] ? rez.pop() : rez.push(masStrFilter[i]);
+                    }
+                }
+            }
+        });
+        i++;
+    }
+    return !stop && rez.length === 0 ? true : false;
 }
